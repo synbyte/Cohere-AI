3e06df3309ccddab3cf3c3fe831b9ebde48fe813
synbyte <rashnikov2.0@gmail.com>
2024-03-28 16:24:00 +0000

    Changed colors

--- app/page.tsx
+++ app/page.tsx
@@ -6,22 +6,22 @@ export default function Chat() {
   const { messages, input, handleInputChange, handleSubmit, data } = useChat();
  
   return (
-    <div className="p-4">
+    <div className="p-8 border-2 border-black h-screen bg-gradient-to-b from-neutral-100 to-neutral-200">
       <header className="text-center">
-        <h1 className="text-xl">Chat Example</h1>
+        <h1 className="text-xl">Resource AI</h1>
       </header>
-      <div className="flex flex-col justify-between w-full max-w-md mx-auto stretch">
-        <div className="flex-grow overflow-y-auto">
+      <div className="flex flex-col justify-between w-full max-w-md mx-auto h-full">
+        <div id='messages' className="flex-grow overflow-y-auto max-h-96 my-4 rounded bg-neutral-100 border-t-2 border-b-2 border-b-gray-300">
           {messages.map(m => (
             <div key={m.id} className="whitespace-pre-wrap">
-              {m.role === 'user' ? 'User: ' : 'AI: '}
+              <span className="text-gray-500">{m.role === 'user' ? 'User: ' : 'AI: '}</span>
               {m.content}
             </div>
           ))}
         </div>
         <form onSubmit={handleSubmit}>
           <input
-            className="fixed bottom-0 w-full max-w-md p-2 mb-8 border border-gray-300 rounded shadow-xl"
+            className="w-full max-w-md p-2 mb-4 border border-gray-300 rounded shadow-xl mx-auto"
             value={input}
             placeholder="Say something..."
             onChange={handleInputChange}
