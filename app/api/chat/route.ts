dc8cbd01f25ec07a7e9a0fb983949a35f7e8198d
synbyte <81550106+synbyte@users.noreply.github.com>
2024-03-27 21:37:48 -0700

    Initial commit

---
+++ app/api/chat/route.ts
@@ -0,0 +1,48 @@
+import { CohereStream, StreamingText } from 'ai';
+import { CohereClient, Cohere } from 'cohere-ai';
+
+export const runtime = "edge"
+
+if (!process.env.COHERE_API_KEY) {
+  throw new Error('Missing COHERE_API_KEY environment variable');
+}
+ 
+const cohere = new CohereClient({
+  token: process.env.COHERE_API_KEY,
+});
+ 
+const toCohereRole = (role: string): Cohere.ChatMessageRole => {
+  if (role === 'user') {
+    return Cohere.ChatMessageRole.User;
+  }
+  return Cohere.ChatMessageRole.Chatbot;
+};
+ 
+export async function POST(req: Request) {
+  // Extract the `prompt` from the body of the request
+  const { messages } = await req.json();
+  const chatHistory = messages.map((message: any) => ({
+    message: message.content,
+    role: toCohereRole(message.role),
+  }));
+  const lastMessage = chatHistory.pop();
+ 
+  const response = await cohere.chatStream({
+    connectors:[{"id": "web-search"}], 
+    message: lastMessage.message,
+    chatHistory,
+  });
+ 
+  const stream = new ReadableStream({
+    async start(controller) {
+      for await (const event of response) {
+        if (event.eventType === 'text-generation') {
+          controller.enqueue(event.text);
+        }
+      }
+      controller.close();
+    },
+  });
+ 
+  return new Response(stream);
+}
