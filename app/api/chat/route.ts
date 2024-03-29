6a3d133e6e9852b4fd2af354170bf0b347b1e550
synbyte <rashnikov2.0@gmail.com>
2024-03-29 16:19:15 +0000

    formated preamble

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -19,6 +19,7 @@ const toCohereRole = (role: string): Cohere.ChatMessageRole => {
 };
  
 export async function POST(req: Request) {
+  try {
   // Extract the `prompt` from the body of the request
   const { messages } = await req.json();
   const chatHistory = messages.map((message: any) => ({
@@ -28,13 +29,17 @@ export async function POST(req: Request) {
   const lastMessage = chatHistory.pop();
   const preamble = `
   ## Task & Context
-  You are Larry, a helpful ai assistant for Pier360.
+  You are PeerAssist, a helpful ai assistant for Pier360.
   Pier360 is a peer support organization and your job is to help individuals find local resources such as; housing for homeless and low income, food banks, mental health treatment, substance abuse treatment, child care, and more. 
   Only list resource in Vancouver, Wa that are listed here: https://www.councilforthehomeless.org/clark-county-resource-guide/. 
   Always start a conversation saying welcome to Pier360, and asking how you can support them. Do not start listing resources until asked to.
   
   ## Style Guide
-  List resources with bullet points. Always show resource name in bold letters. Always include the phone number and url.
+  List resources with markdown bullet points and in this format:
+  - Bold title, description
+  (website)
+  (location)
+  (phone number)
   `
   const response = await cohere.chatStream({
     connectors: [{"id":"web-search","options":{"site":"https://www.councilforthehomeless.org/clark-county-resource-guide/#1630624957895-e37b54c6-6479"}}],
@@ -54,6 +59,8 @@ export async function POST(req: Request) {
     },
   });
   
-
   return new StreamingTextResponse(stream);
+} catch (error) {
+  console.error("Caught Error: ,", error);
+}
 }
