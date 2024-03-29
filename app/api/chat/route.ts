1c65a8adae368142fb05587a448fe4d0621a077e
synbyte <rashnikov2.0@gmail.com>
2024-03-29 05:20:48 +0000

    Styling changes, fonts, preamble const.

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -26,19 +26,19 @@ export async function POST(req: Request) {
     role: toCohereRole(message.role),
   }));
   const lastMessage = chatHistory.pop();
- 
+  const preamble = `
+  ## Task & Context
+  You are Larry, a helpful ai assistant for Pier360.
+  Pier360 is a peer support organization and your job is to help individuals find local resources such as; housing for homeless and low income, food banks, mental health treatment, substance abuse treatment, child care, and more. 
+  Only list resource in Vancouver, Wa that are listed here: https://www.councilforthehomeless.org/clark-county-resource-guide/. 
+  Always start a conversation saying welcome to Pier360, and asking how you can support them. Do not start listing resources until asked to.
+  
+  ## Style Guide
+  List resources with bullet points. Always show resource name in bold letters. Always include the phone number and url.
+  `
   const response = await cohere.chatStream({
     connectors: [{"id":"web-search","options":{"site":"https://www.councilforthehomeless.org/clark-county-resource-guide/#1630624957895-e37b54c6-6479"}}],
-    preamble: `
-    ## Task & Context
-    You are Larry, a helpful ai assistant for Pier360.
-    Pier360 is a peer support organization and your job is to help individuals find local resources such as; housing for homeless and low income, food banks, mental health treatment, substance abuse treatment, child care, and more. 
-    Only list resource in Vancouver, Wa that are listed here: https://www.councilforthehomeless.org/clark-county-resource-guide/. 
-    Always start a conversation saying welcome to Pier360, and asking how you can support them.
-    
-    ## Style Guide
-    List resources using bullet points. Always show resource name in bold letters. Always include the phone number and url.
-    `, 
+    preamble: preamble, 
     message: lastMessage.message,
     chatHistory,
   });
--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,30 @@
 'use client';
-
+import { useEffect } from 'react';
+import { Raleway } from 'next/font/google';
 import { useChat } from 'ai/react';
 import Markdown from 'react-markdown';
+
+
 export default function Chat() {
   const { messages, input, handleInputChange, handleSubmit, data } = useChat();
-
+  useEffect(() => {
+    const chatContainer = document.getElementById('messages');
+    chatContainer.scrollTop = chatContainer?.scrollHeight;
+  }, [messages]);
   return (
-    <div className="p-8 border-2 border-black h-screen bg-gradient-to-b from-neutral-100 to-neutral-300">
+    <div className="p-8 border-2 border-black h-full bg-gradient-to-b from-neutral-100 to-neutral-300">
       <header className="text-center">
-        <div className="text-2xl font-extrabold ...">
+        <div className="text-xl font-extrabold ...">
           <span className="bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500">
-            LarrY AI
+            PeerAssist
           </span>
         </div>
       </header>
       <div className="flex flex-col justify-between w-full max-w-md mx-auto h-full">
-        <div id='messages' className="flex-grow overflow-y-auto max-h-96 my-4 rounded bg-neutral-100 border-t-2 border-b-2 border-b-gray-300">
+        <div id='messages' className="flex-grow overflow-y-scroll max-h-96 my-4 rounded bg-neutral-100 border-t-2 border-b-2 border-b-gray-300">
           {messages.map(m => (
             <div key={m.id} className="whitespace-pre-wrap p-1">
-              <span className="text-blue-500">{m.role === 'user' ? 'You: ' : 'AI: '}</span>
+              {m.role === 'user' ? <span className="text-fuchsia-600">You: </span> : <span className="text-blue-700">AI: </span>}
               <Markdown>{m.content}</Markdown>
             </div>
           ))}
@@ -27,7 +33,7 @@ export default function Chat() {
           <input
             className="w-full max-w-md p-2 mb-4 border border-gray-300 rounded shadow-xl mx-auto"
             value={input}
-            placeholder="Say something..."
+            placeholder="What can I help you find?"
             onChange={handleInputChange}
           />
         </form>
