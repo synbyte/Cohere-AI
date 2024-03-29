da27ae2498ee5dc2efd33c58977278c8185c5baf
synbyte <rashnikov2.0@gmail.com>
2024-03-29 18:19:14 +0000

    changed messages window to min-h-96 and added bottom padding of 20

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,7 @@ export default function Chat() {
   }, [messages]);
   
   return (
-    <div className="p-8 border-2 border-black h-full bg-gradient-to-b from-neutral-100 to-neutral-300">
+    <div className="p-8 pb-20 border-2 h-full bg-gradient-to-b from-neutral-100 via-cyan-100 to-neutral-300">
       <header className="text-center">
         <div className="text-xl font-extrabold ...">
           <span className="bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500">
@@ -28,8 +28,8 @@ export default function Chat() {
         </div>
       </header>
       <Dialog id='dialog' open={isOpen} onClose={() => setIsOpen(false)}>
-      <div className='fixed inset-0 flex w-screen items-center justify-center p-4 '>
-      <Dialog.Panel className="w-full max-w-sm rounded-xl bg-neutral-200 p-5 shadow-2xl">
+      <div className='fixed inset-0 flex w-screen text-center items-center justify-center p-4 '>
+      <Dialog.Panel className="w-full max-w-sm rounded-xl bg-neutral-300 p-5 shadow-2xl">
         <Dialog.Title>Welcome to PeerAssist</Dialog.Title>
         <Dialog.Description>
           
@@ -39,13 +39,13 @@ export default function Chat() {
           This AI is still in development. Please let me know of any bugs or inconsistencies that may come up.</p>
           <br/><p>Start by typing what you&#39;re looking for; <i>&quot;I need housing resources&quot;, &quot;What services do you have for veterans?&quot;, &quot;I need a hot meal&quot;.</i> Or just simply say hi!</p>
 
-        <button className='rounded-lg shadow-md bg-neutral-300 p-2' onClick={() => setIsOpen(false)}>Close</button>
+        <button className='rounded-lg shadow-md bg-neutral-200 p-1 m-2' onClick={() => setIsOpen(false)}>Close</button>
         
       </Dialog.Panel>
       </div>
     </Dialog>
       <div className="flex flex-col justify-between w-full max-w-md mx-auto h-full">
-        <div id='messages' className="flex-grow overflow-y-scroll max-h-96 my-4 rounded bg-neutral-100 border-t-2 border-b-2 border-b-gray-300">
+        <div id='messages' className="flex-grow overflow-y-scroll min-h-96 my-4 rounded bg-neutral-100 border-t-2 border-b-2 border-b-gray-300">
           {messages.map(m => (
             <div key={m.id} className="whitespace-pre-wrap p-1">
               {m.role === 'user' ? <span className="text-fuchsia-600">You: </span> : <span className="text-blue-700">AI: </span>}
