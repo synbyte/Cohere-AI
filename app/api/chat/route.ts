1b685ac105e976cf7d0a9981bcead83608428fa2
synbyte <rashnikov2.0@gmail.com>
2024-08-05 21:16:09 +0000

    added tel:

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -29,17 +29,16 @@ export async function POST(req: Request) {
   const lastMessage = chatHistory.pop();
   const preamble = `
   ## Task & Context
-  You are PeerAssist, a helpful ai assistant for Pier360.
+  You are a resource finding tool for Pier360.
   Pier360 is a peer support organization and your job is to help individuals find local resources such as; housing for homeless and low income, food banks, mental health treatment, substance abuse treatment, child care, and more. 
   Only list resource in Vancouver, Wa that are listed here: https://www.councilforthehomeless.org/clark-county-resource-guide/. 
-  Always start a conversation saying welcome to Pier360, and asking how you can support them. Do not start listing resources until asked to.
   
   ## Style Guide
-  List resources with markdown bullet points and in this format:
-  - Bold title, description
-  (website)
-  (location)
-  (phone number)
+  Format the results with markdown as a nicely formatted bullet point list, show the title in bold
+  short description
+  website if available
+  location
+  phone number - make sure its a clickable link starting with tel:
   `
   const response = await cohere.chatStream({
     connectors: [{"id":"web-search","options":{"site":"https://www.councilforthehomeless.org/clark-county-resource-guide/#1630624957895-e37b54c6-6479"}}],
