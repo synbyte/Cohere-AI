e3068d05a8db7c0ae1deb81f59ab878f5d42975d
synbyte <81550106+synbyte@users.noreply.github.com>
2024-08-09 10:55:00 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: "/Cohere-AI",//This,
-  output: "export", //This and..
-  reactStrictMode: true, //This is for deploying to github pages. Remove to revert back.
 };
 
 module.exports = nextConfig;
